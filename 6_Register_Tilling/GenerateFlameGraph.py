fc8878b63b9c5e4c6dd10f9235775794c8d561da
YanivDoron <103060364+YanivDoron@users.noreply.github.com>
2025-07-22 14:06:52 +0300

    Update GenerateFlameGraph.py

---
+++ 6_Register_Tilling/GenerateFlameGraph.py
@@ -0,0 +1,35 @@
+import torch
+from torch.profiler import profile, record_function, ProfilerActivity
+import matmul # Your compiled CUDA extension
+
+TRACE_PATH = "regtile.json"
+
+def matmul_naive(A1, A2):
+    return matmul.matmul(A1.contiguous(), A2.contiguous())
+
+
+def main():
+    N = 2048
+    A = torch.randn(N, N, device='cuda')
+    B = torch.randn(N, N, device='cuda')
+
+    # Warm-up (important for accurate profiling)
+    for _ in range(1):
+        matmul.matmul(A.contiguous(), B.contiguous())
+
+
+    with profile(
+        activities=[ProfilerActivity.CPU, ProfilerActivity.CUDA],
+        record_shapes=True,
+        with_stack=True,
+        profile_memory=True
+    ) as prof:
+        with record_function("matmul_naive"):
+            matmul_naive(A, B)
+
+    # Export trace to Chrome JSON format
+    prof.export_chrome_trace(TRACE_PATH)
+    print(f"✅ Trace saved to: {TRACE_PATH}")
+
+if __name__ == "__main__":
+    main()
